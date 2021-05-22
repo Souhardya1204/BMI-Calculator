9fd6106b2d37838137c763932113a50f4905672c
Souhardya Mandal <souhardya1204@gmail.com>
2021-05-22 22:14:04 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 06:27:24 2021
+
+@author: Souhardya
+"""
+
+from flask import Flask, request,render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def welcome():
+    bmi=''
+    if request.method=='POST' and 'height' in request.form:
+        height = float(request.form.get('height'))
+        weight = float(request.form.get('weight'))
+        bmi =CalculateBMI(height , weight)
+    return render_template('index.html',
+                            bmi=bmi)
+
+def CalculateBMI(h,w):
+    bmi=(w/(h*h))*10000
+    return round(bmi,2)
+
+app.run(debug=True)
+
+
+
